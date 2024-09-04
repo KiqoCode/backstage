4da14bf5fbef638b4001901010a441e5ed29fab5
Cisco Osterman <cisco.osterman@tre.se>
2024-09-04 15:21:03 +0200

    Initial Commit

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,117 @@
+/*
+ * Hi!
+ *
+ * Note that this is an EXAMPLE Backstage backend. Please check the README.
+ *
+ * Happy hacking!
+ */
+
+import { createBackend } from '@backstage/backend-defaults';
+
+const backend = createBackend();
+
+backend.add(import('@backstage/plugin-app-backend/alpha'));
+backend.add(import('@backstage/plugin-proxy-backend/alpha'));
+backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
+backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
+
+// auth plugin
+backend.add(import('@backstage/plugin-auth-backend'));
+backend.add(import('@backstage/plugin-auth-backend-module-microsoft-provider'));
+// See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
+// backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
+// See https://backstage.io/docs/auth/guest/provider
+
+import { stringifyEntityRef, DEFAULT_NAMESPACE } from '@backstage/catalog-model';
+import { createBackendModule } from '@backstage/backend-plugin-api';
+import { microsoftAuthenticator } from '@backstage/plugin-auth-backend-module-microsoft-provider';
+import {
+  authProvidersExtensionPoint,
+  createOAuthProviderFactory,
+} from '@backstage/plugin-auth-node';
+
+const customAuth = createBackendModule({
+  // This ID must be exactly "auth" because that's the plugin it targets
+  pluginId: 'auth',
+  // This ID must be unique, but can be anything
+  moduleId: 'custom-auth-provider',
+  register(reg) {
+    reg.registerInit({
+      deps: { providers: authProvidersExtensionPoint },
+      async init({ providers }) {
+        providers.registerProvider({
+          // This ID must match the actual provider config, e.g. addressing
+          // auth.providers.github means that this must be "github".
+          providerId: 'microsoft',
+          // Use createProxyAuthProviderFactory instead if it's one of the proxy
+          // based providers rather than an OAuth based one
+          factory: createOAuthProviderFactory({
+            authenticator: microsoftAuthenticator,
+            async signInResolver({ profile }, ctx) {
+              if (!profile.email) {
+                throw new Error(
+                  'Login failed, user profile does not contain an email',
+                );
+              }
+              // Split the email into the local part and the domain.
+              const [localPart, domain] = profile.email.split('@');
+            
+              // Next we verify the email domain. It is recommended to include this
+              // kind of check if you don't look up the user in an external service.
+              if (domain !== 'tre.se') {
+                throw new Error(
+                  `Login failed, '${profile.email}' does not belong to the expected domain`,
+                );
+              }
+            
+              // By using `stringifyEntityRef` we ensure that the reference is formatted correctly
+              const userEntity = stringifyEntityRef({
+                kind: 'User',
+                name: localPart,
+                namespace: DEFAULT_NAMESPACE,
+              });
+              return ctx.issueToken({
+                claims: {
+                  sub: userEntity,
+                  ent: [userEntity],
+                },
+              });
+            }
+          }),
+        });
+      },
+    });
+  },
+});
+
+// catalog plugin
+backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+backend.add(
+  import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
+);
+
+// See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
+backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
+
+// permission plugin
+backend.add(import('@backstage/plugin-permission-backend/alpha'));
+// See https://backstage.io/docs/permissions/getting-started for how to create your own permission policy
+backend.add(
+  import('@backstage/plugin-permission-backend-module-allow-all-policy'),
+);
+
+// search plugin
+backend.add(import('@backstage/plugin-search-backend/alpha'));
+
+// search engine
+// See https://backstage.io/docs/features/search/search-engines
+backend.add(import('@backstage/plugin-search-backend-module-pg/alpha'));
+
+// search collators
+backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
+backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
+
+// kubernetes
+backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
+
+backend.start();
